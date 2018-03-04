ef9e9b99cb3468a9e5b1595091aa83d4c20807c1
Mathieu <mathieu.abeille@gmail.com>
2018-03-04 19:04:36 +0100

    v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  devise_scope :user do
+  root :to => 'devise/registrations#new'
+end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
